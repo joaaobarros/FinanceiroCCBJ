1ce1fc28afc9ea3c8916618504e5b345d59464a0
joaaobarros <jpbarros.boletos@gmail.com>
2025-04-20 23:38:06 -0300

    Update urls.py

--- backend/ccbj_financeiro/urls.py
+++ backend/ccbj_financeiro/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls) ,
     path('api/', include('core.urls')),
+    path('', include('core.urls')),  # Esta linha é importante para a rota raiz
 ]
